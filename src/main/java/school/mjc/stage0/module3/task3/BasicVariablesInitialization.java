dd7b0d2a07818ef7786c2b70035b577367f0d2cd
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 12:33:31 +0600

    Update BasicVariablesInitialization.java

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -3,5 +3,9 @@ package school.mjc.stage0.module3.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         //write code here
+        int first = 1, second = 10, third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
