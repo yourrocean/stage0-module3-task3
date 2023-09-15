91c7b5c7899019d689ae414af9108b8b2903671e
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 12:40:14 +0600

    Update ReassigningValues.java

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,5 +3,16 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         //write code here
+        int first = 1, second = 10, third = 100, linkToFirst, linkToSecond, linkToThird;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        linkToFirst = first;
+        linkToSecond = second;
+        linkToThird = third;
+        first = 15;
+        second = 6;
+        third = 4;
+        
     }
 }
